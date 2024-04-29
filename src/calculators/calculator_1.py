572fdfaaf147623249024db4e2a879f9e8cb35d4
mgckaled <marcelgckaled@gmail.com>
2024-04-29 20:00:53 -0300

    feat: implement initial calculator logics

---
+++ src/calculators/calculator_1.py
@@ -0,0 +1,26 @@
+from typing import Dict
+
+from flask import Request
+
+
+class Calculator1:
+
+    def calculate(self, request: Request) -> Dict:
+        body = request.json
+        input_data = self.__validate_body(body)
+        splited_number = input_data / 3
+
+        first_process_result = self.__first_process(splited_number)
+        print(first_process_result)
+
+    def __validate_body(self, body: Dict) -> float:
+        if "number" not in body:
+            raise Exception("body mal formatado!")
+
+        input_data = body["number"]
+        return input_data
+
+    def __first_process(self, first_number: float) -> float:
+        first_part = (first_number / 4) + 7
+        second_part = (first_part ** 2) * 0.257
+        return second_part
