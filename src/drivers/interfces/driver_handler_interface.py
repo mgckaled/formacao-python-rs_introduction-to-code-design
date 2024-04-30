433e0bade713614a1b9508417b2bc670fbfa51f0
mgckaled <marcelgckaled@gmail.com>
2024-04-30 11:18:24 -0300

    feat: create abstract class - interface

---
+++ src/drivers/interfces/driver_handler_interface.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class DriverHandlerInterface(ABC):
+
+    @abstractmethod
+    def standard_derivation(self, number: List[float]) -> float:
+        pass
+
+    @abstractmethod
+    def variance(self, number: List[float]) -> float:
+        pass
